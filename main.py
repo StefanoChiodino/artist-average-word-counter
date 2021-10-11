eb88979e942bead96d59ac9d00005e28a97da83f
Stefano Chiodino <stefano@nope.com>
2021-10-11 19:53:11 +0100

    quick save before using musicbrainz own library

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import argparse
 
 import urllib.parse
-from typing import Optional
+from typing import Optional, List
 
 import requests
 
@@ -10,7 +10,8 @@ MUSICBRAINZ_DEFAULT_PARAMETERS = {"fmt": "json"}
 DEBUG_LOGGING = True
 
 
-def musicbrainz_artist_id(artist: str) -> Optional[str]:
+def lookup_artist_id(artist: str) -> Optional[str]:
+    """ Find an artist ID from the name. """
     url = artist_lookup_url(artist)
     response = requests.get(url)
     artists = response.json().get("artists")
@@ -21,9 +22,31 @@ def musicbrainz_artist_id(artist: str) -> Optional[str]:
         if DEBUG_LOGGING:
             print(f"Picking the most relevant '{artist['type']}' with id '{artist['id']}', called "
                   f"'{artist['name']}', from '{artist['country']}', with confidence {artist['score']}%")
+
+        # TODO: return the list and prompt the user to pick the right one. Maybe only if score < 100.
         return artist["id"]
 
     return None
+
+
+def releases(artist_id: str, chunk_by: int = 100) -> List[str]:
+    """ Find all releases by an artist. """
+    releases_ids: List[str] = []
+    while True:
+        url = release_search_url(artist_id, limit=chunk_by)
+        response = requests.get(url)
+        current_releases = response.json().get("releases")
+        if current_releases:
+            releases_ids += [x["id"] for x in current_releases]
+            if DEBUG_LOGGING:
+                print(f"Fetched {len(current_releases)} releases.")
+
+        if not current_releases or len(current_releases) < chunk_by:
+            break
+
+    if DEBUG_LOGGING:
+        print(f"Finished fetching {len(releases_ids)} releases.")
+    return releases_ids
 
 
 def artist_lookup_url(artist: str) -> str:
@@ -32,8 +55,19 @@ def artist_lookup_url(artist: str) -> str:
     return f"{url}?{query}"
 
 
+def release_search_url(artist_id: str, limit: int = None, offset: int = None) -> str:
+    url = urllib.parse.urljoin(MUSICBRAINZ_URL, f"release")
+    parameters = {"artist": artist_id}
+    if limit is not None:
+        parameters["limit"] = limit
+    if offset is not None:
+        parameters["offset"] = offset
+    query = urllib.parse.urlencode({**parameters, **MUSICBRAINZ_DEFAULT_PARAMETERS})
+    return f"{url}?{query}"
+
+
 def average_lyrics_words(artist: str):
-    artist_id = musicbrainz_artist_id(artist)
+    artist_id = lookup_artist_id(artist)
     pass
 
 
--- main_end_to_end_test.py
+++ main_end_to_end_test.py
@@ -1,11 +1,16 @@
 import unittest
 
-from main import musicbrainz_artist_id
+from main import lookup_artist_id, releases
 
 
 class ArtistAverageWordCounterTest(unittest.TestCase):
-    def test_musicbrainz_artist_id(self):
-        artist_id = musicbrainz_artist_id("metallica")
+    def test_lookup_artist_id(self):
+        artist_id = lookup_artist_id("metallica")
+
+        self.assertIsNotNone(artist_id)
+
+    def test_releases(self):
+        artist_id = releases("65f4f0c5-ef9e-490c-aee3-909e7ae6b2ab")
 
         self.assertIsNotNone(artist_id)
 
